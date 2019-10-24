43b6b55a69c38b19f932d35c3ad365e26eddafc5
Isaque Alves <isaquealves@gmail.com>
2019-10-24 01:07:26 -0300

    Add Initial client facade implementation

---
+++ getresponse_api_wrapper/client.py
@@ -0,0 +1,19 @@
+"""GetResponse client module
+"""
+from getresponse_api_wrapper.settings import Config
+from getresponse_api_wrapper.request import Request
+from getresponse_api_wrapper.resources.accounts import Accounts
+
+
+class GetResponseClient:
+    """GetResponseClient
+    """
+
+    def __init__(self, config: Config = Config()):
+        self._config = config
+        self.request = Request(self._config)
+
+    def accounts(self):
+        return Accounts(self.request)
+
+
